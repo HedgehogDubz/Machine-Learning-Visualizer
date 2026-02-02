b022e41bc2e14d0c636a8899e2cc04f1614b99b7
HedgehogDubz <tristankenshin@gmail.com>
2026-02-02 15:43:46 -0500

    mutations added

--- neuralnetworkvisualizer/src/neuralnetwork.ts
+++ neuralnetworkvisualizer/src/neuralnetwork.ts
@@ -5,12 +5,16 @@ export class NeuralNetwork {
     inputSize: number;
     hiddenLayerSizes: number[];
     outputSize: number;
-    startRandomBias: boolean = true;
-    startRandomWeights: boolean = true;
-    linePower = 4;
+    startRandomBias: boolean = false;
+    startRandomWeights: boolean = false;
+    clampBiases: boolean = true;
+    clampWeights: boolean = true;
+
+    changeWeightAlpha: boolean = false;
+    linePower: number = 4;
 
     layers: Layer[] = [];
-    activationFunction: ActivationFunction = 'sigmoid';
+    activationFunction: ActivationFunction = 'tanh';
     constructor(inputSize: number, hiddenLayerSizes: number[], outputSize: number, activationFunction: ActivationFunction) {
         this.numOfLayers = hiddenLayerSizes.length + 2;
         this.inputSize = inputSize
@@ -19,16 +23,75 @@ export class NeuralNetwork {
         this.initLayers();
         this.activationFunction = activationFunction;
     }
+    public run(inputs: number[]) {
+        if (inputs.length != this.inputSize) {
+            throw new Error('Wrong Number of INPUTs, Expected: ' + this.inputSize + "| Received: " + inputs.length)
+        }
+        for (let i = 0; i < this.inputSize; i++){
+            this.layers[0].neurons[i].value = inputs[i];
+        }
+        for (let l = 1; l < this.numOfLayers; l++){
+            let neurons = this.layers[l].neurons;
+            for (let n = 0; n < neurons.length; n++){
+                let neuron = neurons[n];
+                neuron.value = 0;
+                let weights = neuron.weights;
+                for (let w = 0; w < weights.length; w++){
+                    let weight: Weight = weights[w];
+                    neuron.value += weight.value * this.getNodeValue(weight.to);
+                }
+                neuron.value += neuron.bias;
+                neuron.value = this.activate(neuron.value);
+                
+            }
+        }
+    }
+    public mutate(numOfWeights:number, weightStrength:number, numOfBiases:number, biasesStrength:number){
+        for (let i = 0; i < numOfWeights; i++){
+            let randLayerIndex = Math.floor(Math.random() * (this.numOfLayers - 1)) + 1;
+            let neurons = this.layers[randLayerIndex].neurons
+            let randNeuronIndex = Math.floor(Math.random() * neurons.length)
+            let weights = this.layers[randLayerIndex].neurons[randNeuronIndex].weights
+            let randWeightsIndex = Math.floor(Math.random() * weights.length);
+            weights[randWeightsIndex].value += (Math.random() * 2 - 1) * weightStrength;
+            if (this.clampWeights){
+                weights[randWeightsIndex].value = Math.min(Math.max(weights[randWeightsIndex].value, -1), 1)
+            }
+        }
+        for (let i = 0; i < numOfBiases; i++){
+            let randLayerIndex = Math.floor(Math.random() * this.numOfLayers);
+            let neurons = this.layers[randLayerIndex].neurons
+            let randNeuronIndex = Math.floor(Math.random() * neurons.length)
+            neurons[randNeuronIndex].bias += (Math.random() * 2 - 1) * biasesStrength;
+            if (this.clampBiases){
+                neurons[randNeuronIndex].bias = Math.min(Math.max(neurons[randNeuronIndex].bias, -1), 1)
+            }
+        }
+    }
+    public getNodeValue(np: NeuronPosition): number {
+        return this.layers[np.layer].neurons[np.index].value;
+    }
+    public activate(n: number){
+        switch (this.activationFunction){
+            case "relu":
+                return (n <= 0)? 0: n;
+            case "sigmoid":
+                return 1 / (1 + Math.pow(Math.E, -n));
+            case "tanh":
+                return Math.tanh(n);
+
+        }
+    }
     private initLayers(): void {
         this.initNextLayer(this.inputSize, false);
         for (let i = 0; i < this.hiddenLayerSizes.length; i++) {
-            this.initNextLayer(this.hiddenLayerSizes[i], true)
+            this.initNextLayer(this.hiddenLayerSizes[i], this.startRandomBias)
         }
-        this.initNextLayer(this.outputSize, false);
+        this.initNextLayer(this.outputSize, this.startRandomBias);
     }
     private initNextLayer(numOfNeurons: number, randomBias: boolean) {
         const layerNeurons: Neuron[] = [];
-
+        console.log(randomBias);
         for (let j = 0; j < numOfNeurons; j++) {
             const neuronWeights: Weight[] = this.initWeights();
 
@@ -54,14 +117,12 @@ export class NeuralNetwork {
                 str += (neuron.bias)
                 str += " "
             }
-            console.log(str);
         }
     }
     public draw(ctx: CanvasRenderingContext2D, left: number, top: number, width: number, height: number) {
         let spaceX = width / (this.numOfLayers + 1);
         let lastYs: number[] = [];
 
-        let lastSpaceY = 0;
         //weights
         for (let i = 0; i < this.numOfLayers; i++) {
             let x: number = (i + 1) * spaceX + left;
@@ -81,7 +142,7 @@ export class NeuralNetwork {
                     ctx.save()
                     ctx.strokeStyle = this.numToColorBlack(neuron.weights[w].value);
                     ctx.lineWidth = 5;
-                    ctx.globalAlpha = Math.abs(neuron.weights[w].value)**this.linePower;
+                    if (this.changeWeightAlpha) ctx.globalAlpha = Math.abs(neuron.weights[w].value)**this.linePower;
                     ctx.beginPath();
                     ctx.moveTo(x, y);
                     ctx.lineTo(lastX, lastYs[w]);
@@ -92,7 +153,6 @@ export class NeuralNetwork {
 
             }
             lastYs = newYs;
-            lastSpaceY = spaceY;
 
 
         }
@@ -118,20 +178,38 @@ export class NeuralNetwork {
                 drawCircle(ctx, { x, y }, r2);
                 ctx.restore();
 
+ 
+
                 ctx.save();
                 let val = Math.floor(neuron.value * 1000) / 1000;
                 let textStr = val.toString();
-                ctx.font = '12px sans-serif';
-                let measure = ctx.measureText(textStr);
-                let w = measure.width;
-                let h = measure.emHeightAscent;
-                ctx.fillText(textStr, x - w / 2,  y + h / 2);
-                ctx.restore();
+                let drawn: boolean = false;
+                while (textStr.length > 0 && textStr != "-" && !drawn){
+                    if (textStr.at(-1) == "."){
+                        textStr = textStr.slice(0, -1);
+                        continue;
+                    }
+                    ctx.font = '12px sans-serif';
+                    let measure = ctx.measureText(textStr);
+                    let w = measure.width;
+                    let h = measure.emHeightAscent;
+
+                    if(w >= r2 * 2 || h >= r2 * 2) {
+                        textStr = textStr.slice(0, -1);
+                        continue;
+                    }
+                    
+                    drawn = true;
+                    ctx.fillText(textStr, x - w / 2,  y + h / 2);
+                    ctx.restore();
+
+                }
 
             }
         }
     }
-    numToColorBlack(num: number): string {
+    
+    private numToColorBlack(num: number): string {
         if (num >= 0) {
             return "rgb(0, " + (Math.tanh(num)) * 255 + ",0)"
         }
@@ -141,7 +219,7 @@ export class NeuralNetwork {
         }
         return ""
     }
-    numToColorWhite(num: number): string {
+    private numToColorWhite(num: number): string {
         if (num >= 0) {
             return "rgb(" + (1 - Math.tanh(num)) * 255 + ", 255," + (1 - Math.tanh(num)) * 255 + ")"
         }
